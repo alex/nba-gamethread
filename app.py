a5fa5965c1668cd70445f3f1bdbc86cab27b4f46
Alex Gaynor <alex.gaynor@gmail.com>
2012-01-28 16:56:38 -0500

    run on a specific port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from collections import namedtuple
 from datetime import date, time, timedelta
 
@@ -147,4 +148,5 @@ def generate():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
