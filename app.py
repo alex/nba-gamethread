1b86a318e8ae8d0ab78de657321ad0feefb31ff2
Alex Gaynor <alex.gaynor@gmail.com>
2015-01-04 16:32:39 -0800

    Testing

--- app.py
+++ app.py
@@ -327,6 +327,12 @@ def generate():
     )
 
 
+@app.route("/test-error/")
+@handle_errors
+def test_error():
+    raise ValueError("test")
+
+
 def configure_raven(app):
     if 'SENTRY_DSN' in os.environ:
         import raven
